c1c3661da82984e438a10a1cf732db03acd012af
UcJunE <ucjune90@gmail.com>
2022-12-26 18:23:05 +0000

    Added route to handle update on products

--- routes/products.js
+++ routes/products.js
@@ -50,4 +50,71 @@ router.post("/create", async (req, res) => {
   });
 });
 
+//handling update. to display
+router.get("/:product_id/update", async (req, res) => {
+  const product_id = req.params.product_id;
+
+  const product = await Jewelry.where({
+    id: product_id,
+  }).fetch({
+    require: true,
+  });
+
+  let colors = await Color.fetchAll().map((color) => {
+    return [color.get("id"), color.get("name")];
+  });
+  const productForm = createProductForm(colors);
+
+  // fill in the existing values
+  productForm.fields.name.value = product.get("name");
+  productForm.fields.description.value = product.get("description");
+  productForm.fields.cost.value = product.get("cost");
+  productForm.fields.design.value = product.get("design");
+  productForm.fields.color_id.value = product.get("color_id");
+  productForm.fields.weight.value = product.get("weight");
+  productForm.fields.width.value = product.get("width");
+  productForm.fields.height.value = product.get("height");
+  productForm.fields.stock.value = product.get("stock");
+
+  res.render("products/update", {
+    form: productForm.toHTML(bootstrapField),
+    product: product.toJSON(),
+  });
+});
+
+//handing update post
+router.post("/:product_id/update", async (req, res) => {
+  const product_id = req.params.product_id;
+
+  const product = await Jewelry.where({
+    id: product_id,
+  }).fetch({
+    require: true,
+  });
+
+  let colors = await Color.fetchAll().map((color) => {
+    return [color.get("id"), color.get("name")];
+  });
+  const productForm = createProductForm(colors);
+
+  productForm.handle(req, {
+    success: async (form) => {
+      // console.log({ ...form.data });
+      console.log(product.get("created_date"));
+      const updateFormData = {
+        ...form.data,
+        created_date: product.get("created_date"),
+      };
+      product.set(updateFormData);
+      await product.save();
+      res.redirect("/products");
+    },
+    error: async (form) => {
+      res.render("products/update", {
+        form: form.toHTML(bootstrapField),
+      });
+    },
+  });
+});
+
 module.exports = router;
